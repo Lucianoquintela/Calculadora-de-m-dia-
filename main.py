643aa300174946682251911336f4fbbd0ee25661
Lucianoquintela0110 <117698113+Lucianoquintela@users.noreply.github.com>
2022-12-03 21:14:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+nota1 = int(input("Digite sua nota do 1° Bimestre:"))
+nota2 = int(input("Digite sua nota do 2° Bimestre:"))
+nota3 = int(input("Digite sua nota do 3° Bimestre:"))
+nota4 = int(input("Digite sua nota do 4° Bimestre:"))
+soma = nota1 + nota2 + nota3 + nota4
+media = soma / 4
+print(f"A sua media e:{media}")
